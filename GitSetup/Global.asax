6712d30401847d8de145eed8b99d8f55c1610d40
drumzjh <drumzjh@hotmail.com>
2015-05-08 18:20:25 +0800

    第一次提交

---
+++ GitSetup/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitSetup.MvcApplication" Language="C#" %>
---
+++ GitSetup/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitSetup.Startup))]
+namespace GitSetup
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
